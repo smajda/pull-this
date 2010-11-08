8a3c61607d80c301d6bc77d58e3995aa174137d9
Jon Smajda <jon@smajda.com>
2010-11-08 11:29:53 -0600

    Works in index views now.
    Added post_id to pull-this id attribute so separate posts with the same
    pull-this id will both be displayed properly in index view.
    
    Thanks to voodoodoodoo:
    
    <http://wordpress.org/support/topic/plugin-pull-this-note-about-display-in-multiple-entries>

--- pull-this.php
+++ pull-this.php
@@ -12,19 +12,28 @@ Version: 1.0
 // shortcode to mark a pull quote: [pullthis id="foo"]Make me a pull quote[/pullthis]
 function pull_this_mark($atts, $content = null) {
   extract(shortcode_atts(array( 'id' => '1', 'display' => 'both'), $atts));
+  $id = pull_this_build_id($id, 'mark');
   if ($display == 'outside' )
-    return '<span class="pull-this-mark" id="pull-this-mark-'.$id.'" style="display:none;">'.$content.'</span>';
+    return '<span class="pull-this-mark" id="'.$id.'" style="display:none;">'.$content.'</span>';
   else 
-    return '<span class="pull-this-mark" id="pull-this-mark-'.$id.'">'.$content.'</span>';
+    return '<span class="pull-this-mark" id="'.$id.'">'.$content.'</span>';
 }
 add_shortcode('pullthis', 'pull_this_mark');
 
 // shortcode to display a pullquote: [pullshow id="foo"]
 function pull_this_show($atts) {
   extract(shortcode_atts(array( 'id' => '1'), $atts));
-  return '<div class="pull-this-show" id="pull-this-show-'.$id.'" style="display:none;"></div>';
+  $id = pull_this_build_id($id, 'show');
+  return '<div class="pull-this-show" id="'.$id.'" style="display:none;"></div>';
 }
 add_shortcode('pullshow', 'pull_this_show');
+
+// prepend pull-this-mark or -show and post_id to id
+function pull_this_build_id($id, $s_or_m = "mark") {
+  $post_id = get_the_ID();
+  $id = "pull-this-".$s_or_m."-".$post_id."-".$id;
+  return $id;
+}
 
 /* Load JS and CSS */
 function pull_this_scripts() {
